a382c7d21732410cedca2b7e4c1a4d2925825cec
tikaramac19 <tikaramac19@gmail.com>
2022-08-18 18:03:24 +0545

    creating shortid and redirct

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const ShortUrl = require('./model/shortUrl')
 
 // connect to the database
 
@@ -10,15 +11,20 @@ mongoose.connect("mongodb://localhost/urlShortner", {
 });
 
 
-
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:false}));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/shortUrl", (req, res) => {});
+app.post("/shortUrl", async(req, res) => {
+   await ShortUrl.create({
+        full: req.body.fullUrl
+    })
+
+    res.redirect('/');
+});
 
 app.listen(8080, () => {
   console.log("server started at http://localhost:8080");
