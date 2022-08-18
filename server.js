efe4ca240b9c3c0cb00fd622cfe5e5c340187d49
tikaramac19 <tikaramac19@gmail.com>
2022-08-18 18:17:52 +0545

    something went wrong

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const ShortUrl = require('./model/shortUrl')
+const ShortUrl = require("./model/shortUrl");
 
 // connect to the database
 
 mongoose.connect("mongodb://localhost/urlShortner", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+
+  res.render("index", { shortUrls: shortUrls });
 });
 
-app.post("/shortUrl", async(req, res) => {
-   await ShortUrl.create({
-        full: req.body.fullUrl
-    })
+app.post("/shortUrl", async (req, res) => {
+  await ShortUrl.create({
+    full: req.body.fullUrl,
+  });
 
-    res.redirect('/');
+  res.redirect("/");
 });
 
 app.listen(8080, () => {
